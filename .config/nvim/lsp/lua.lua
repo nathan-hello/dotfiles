3f70116dcc061c37bdb267c6bd717691a1f3b302
Nathan <nathanhello321@gmail.com>
2026-02-24 13:54:35 -0500

    nvim lsp update

---
+++ .config/nvim/lsp/lua.lua
@@ -0,0 +1,36 @@
+-- https://github.com/LuaLS/lua-language-server
+
+return {
+        cmd = { "lua-language-server" },
+        filetypes = { "lua" },
+        root_markers = {
+                '.luarc.json',
+                '.luarc.jsonc',
+                '.luacheckrc',
+                '.stylua.toml',
+                'stylua.toml',
+                'selene.toml',
+                'selene.yml',
+                '.git',
+        },
+        settings = {
+                Lua = {
+                        diagnostics = {
+                                disable = { "missing-fields" },
+                                globals = { "vim" },
+                        },
+                        workspace = {
+                                -- Make language server aware of runtime files
+                                library = vim.api.nvim_get_runtime_file("", true),
+                        },
+                        hint = {
+                                enable = true,
+                                setType = false,
+                                paramType = true,
+                                paramName = "Disable",
+                                semicolon = "Disable",
+                                arrayIndex = "Disable",
+                        },
+                },
+        },
+}
---
+++ .config/nvim/lsp/typescript.lua
@@ -0,0 +1,9 @@
+-- npm install -g typescript-language-server typescript
+
+local M = {
+        cmd = { "typescript-language-server", "--stdio" },
+        filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+        root_markers = { "package.json", "tsconfig.json" }
+}
+
+return M
